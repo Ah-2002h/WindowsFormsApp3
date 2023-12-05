b3f026dd31d2785aa3c1b6d6fc8ba5e3c55b3ec2
Maged <Maged@DESKTOP-NS5A77L>
2023-12-05 20:06:53 +0300

    lihkjhj

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -46,7 +46,7 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button2.Location = new System.Drawing.Point(78, 99);
+            this.button2.Location = new System.Drawing.Point(318, 70);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -55,7 +55,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(512, 99);
+            this.button3.Location = new System.Drawing.Point(551, 70);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
